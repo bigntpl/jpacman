8fa6535af618fee99194e813ac7a99a2d160732a
bigntpl <nattapon.ler@hotmail.com>
2022-11-21 00:05:32 +0700

    test: fix build failed

--- src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
+++ src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -1,16 +1,14 @@
 package nl.tudelft.jpacman.level;
 import nl.tudelft.jpacman.PacmanConfigurationException;
-import nl.tudelft.jpacman.board.BasicSquare;
 import nl.tudelft.jpacman.board.BoardFactory;
-import nl.tudelft.jpacman.board.Square;
-import nl.tudelft.jpacman.npc.ghost.*;
+import nl.tudelft.jpacman.npc.ghost.Blinky;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 /**
@@ -35,7 +33,7 @@ public class MapParserTest {
         assertNotNull(levelFactory);
 
         Mockito.when(levelFactory.createGhost()).thenReturn(blinky);
-        MapParser mapParser = new MapParser(levelFactory,boardFactory);
+        MapParser mapParser = new MapParser(levelFactory, boardFactory);
         ArrayList<String> map = new ArrayList<>();
         map.add("############");
         map.add("#P        G#");
@@ -44,7 +42,8 @@ public class MapParserTest {
 
         Mockito.verify(levelFactory, Mockito.times(1)).createGhost();
 
-        Mockito.verify(levelFactory, Mockito.times(1)).createLevel(Mockito.any(), Mockito.any(), Mockito.any());
+        Mockito.verify(levelFactory,
+            Mockito.times(1)).createLevel(Mockito.any(), Mockito.any(), Mockito.any());
     }
 
     /**
